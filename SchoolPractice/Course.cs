a90348f8e54c31f439b64a73479a3f7cd4ed5295
Rowan Polster <rowandownriver94@gmail.com>
2021-07-28 18:54:19 -0500

    simplified Program.cs code

---
+++ SchoolPractice/Course.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolPractice
+{
+    public class Course
+    {
+        public string CourseName { get; set; }
+        public int CourseNo { get; set; }
+        public string Teacher { get; set; }
+        public List<string> EnrolledStudents { get; set; } //Key: student's name, Value: student's ID number
+
+    }
+}
--- SchoolPractice/Program.cs
+++ SchoolPractice/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SchoolPractice
 {
@@ -7,6 +8,22 @@ namespace SchoolPractice
         public static void Main(string[] args)
         {
             // TODO: Instantiate your objects and test your exercise solutions with print statements here.
+
+            //new student:
+
+            Student student = new Student() { Name = "Rowan", StudentId = 12345, NumberOfCredits = 1, Gpa = 4.0 };
+
+            List<string> students = new List<string>();
+            students.Add(student.Name);
+
+            //new course:
+
+            Course course = new Course() { CourseName = "Robotics", CourseNo = 101, EnrolledStudents = students, Teacher = "Hodgeson" };
+            
+
+            //new teacher:
+
+            Teacher teacher = new Teacher() { FirstName = "Joel", LastName = "Hodgeson", Subject = course.CourseName, YearsTeaching = 40 }; 
         }
     }
 }
